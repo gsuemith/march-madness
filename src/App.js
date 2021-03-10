4a1e5fc446e705d77ec23682148ee72bfb74716b
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-10 15:17:39 -0500

    created routes folder

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios'
-
 import CharacterList from './components/CharacterList'
-
-import { URL, PUBLIC_KEY, PRIVATE_KEY } from './keys'
+import Home from './routes/Home'
 
 import './App.css';
 
 function App() {
-  // const [characters, setCharacters] = useState([])
-  // useEffect(() => {
-  //   let md5 = require('md5')
-  //   let ts = Date.now();
-  //   const params = {
-  //     "apikey": PUBLIC_KEY,
-  //     "ts": ts,
-  //     "hash": md5(ts + PRIVATE_KEY + PUBLIC_KEY)
-  //   }
-  //   const headers = {
-  //     Accept: '/'
-  //   }
-
-  //   axios.get(`${URL}/events/29/characters?limit=60&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${params.hash}`)
-  //     .then(res => {
-  //       setCharacters(res.data.data.results)
-  //     })
-  //     .catch(err => console.log(err))
-  // }, [])
   return (
     <div className="App">
-      <CharacterList/>
+      <Home/>
     </div>
   );
 }
-// 01 02 03 04 05 06 07 08
-// 16 15 14 13 12 11 10 09
-// 08 07 06 05
-// 04 03
-// 02
+
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import CharacterList from '../components/CharacterList'
+
+const Home = () => {
+  return (
+    <div>
+      <CharacterList />
+    </div>
+  )
+}
+
+export default Home
