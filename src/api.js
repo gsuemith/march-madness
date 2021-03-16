3165eac42ac2a12ce3199b1d0365e6746ba5d1db
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-16 17:40:30 -0400

    set keys to enviornment variables

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
-import { PUBLIC_KEY, PRIVATE_KEY } from './keys'
+// import { PUBLIC_KEY, PRIVATE_KEY } from './keys'
+
+const PUBLIC_KEY = process.env.MARVEL_PUBLIC_KEY;
+const PRIVATE_KEY = process.env.MARVEL_PRIVATE_KEY;
 
 export const URL = 'https://gateway.marvel.com/v1/public/'
 
